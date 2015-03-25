127cb84f45a6d5f522f07594cc4784f9c27a0775
William Yu <williamyu@gnome.org>
2015-03-25 10:30:14 -0400

    Add license information to all the tests

--- tests/timezone.py
+++ tests/timezone.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 #GI_TYPELIB_PATH=$PREFIX/lib/girepository-1.0/ ./timezone.py
+
+###############################################################################
+#
+# Copyright (C) 2015 William Yu <williamyu@gnome.org>
+#
+# This library is free software: you can redistribute it and/or modify it
+# under the terms of version 2.1. of the GNU Lesser General Public License
+# as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
 from gi.repository import ICalGLib
 
 la = ICalGLib.Timezone.get_builtin_timezone ("America/Los_Angeles");
